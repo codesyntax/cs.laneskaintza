42cfc7f7b2dcea9f0f7ef15aebe7815b3d2c7f31
libargutxi <libargutxi@gmail.com>
2009-04-27 10:51:52 +0000

    locales eta optimizazioa
    svn path=/cs.laneskaintza/trunk/; revision=20060

--- cs/laneskaintza/browser/laneskaintza_lista.py
+++ cs/laneskaintza/browser/laneskaintza_lista.py
@@ -13,26 +13,13 @@ class Lista(BrowserView):
 			
 	def laneskaintza_lista(self, situation):
 		context = aq_inner(self.context)
-                #import pdb;pdb.set_trace()
-                laneskaintzak=context.getFolderContents({'portal_type':'laneskaintza','review_state':'published'})
-                lista1=[]
-                lista2=[]
-                lista3=[]
-		for obj in laneskaintzak:
-                    if obj.getObject().getSituation() in ["zabalik","abierto"]:
-                        lista1.append(obj)
-                    elif obj.getObject().getSituation() in ["martxan","en-proceso"]:
-                        lista2.append(obj)
-                    else:
-                        lista3.append(obj)
+                
+                situations=context.getSituation_source()
+                situation_string=situations[situation]
+
+                laneskaintzak=context.getFolderContents({'portal_type':'laneskaintza','review_state':'published','laneskaintza_situation':situation_string})
                 from Products.CMFPlone import Batch
                 b_start = context.REQUEST.get('b_start', 0)
-                if situation==1:
-                    batch = Batch(lista1, 10, int(b_start), orphan=0)
-                    return batch
-                elif situation==2:
-                    batch = Batch(lista2, 10, int(b_start), orphan=0)
-                    return batch
-                else:
-                    batch = Batch(lista3, 10, int(b_start), orphan=0)
-                    return batch
+                batch = Batch(laneskaintzak, 10, int(b_start), orphan=0)
+                return batch
+                
