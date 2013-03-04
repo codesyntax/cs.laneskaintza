a20fae8bdd476ee84a6e33a308637a4d5b1a3e00
Lur Ibargutxi <libargutxi@codesyntax.com>
2013-03-04 16:34:32 +0100

    delete imports

--- cs/laneskaintza/interfaces/laneskaintza.py
+++ cs/laneskaintza/interfaces/laneskaintza.py
@@ -1,9 +1,6 @@
 from zope import schema
 from zope.interface import Interface
 
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
-
 from cs.laneskaintza import laneskaintzaMessageFactory as _
 
 class Ilaneskaintza(Interface):
